8ebfdd892f78770c2172579343d975aa077e98f3
donggonyoo <ddkk8525@gmail.com>
2025-04-13 17:28:37 +0900

    테스트입니다!

---
+++ donggon/src/rock_paper_scissors/ThisisTest.java
@@ -0,0 +1,13 @@
+package rock_paper_scissors;
+
+public class ThisisTest {
+
+	public static void main(String[] args) {
+		System.out.println("테스트할겁니다!!!!");
+		int a = 100;
+		int b = 100;
+		System.out.println(a+b);
+		
+	}
+
+}
